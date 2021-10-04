33f1517dde2bd950a80097793af4533337600631
Maxim Novikov1 <maxim_novikov1@epam.com>
2021-10-04 18:48:55 +0300

    refactor: change api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev/products/',
+  product: 'https://bgvky3n610.execute-api.eu-west-1.amazonaws.com/dev/products/',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev/products/',
   import: 'https://z6c35vjv1d.execute-api.eu-west-1.amazonaws.com/dev/import',
-  bff: 'https://hre1jg2q5j.execute-api.eu-west-1.amazonaws.com/dev/products/',
+  bff: 'https://bgvky3n610.execute-api.eu-west-1.amazonaws.com/dev/products/',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev/products/',
 };
 
